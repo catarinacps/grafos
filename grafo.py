067df45d2864a584ffca57eca648b473f158d9df
Henrique Silva <henrique.p.silva@outlook.com>
2017-12-09 19:26:46 +0000

    agora com prints bunitus e uns comentarios

--- grafo.py
+++ grafo.py
@@ -1,101 +1,118 @@
 import re
+from text_format import Color
 
-INF = 1000000
+INF = 1001
 INF_CHAR = u'\u221E'
 
 
 class Grafo:
+    """A classe Grafo representa um grafo conexo.
+
+    Deve receber como entrada de seu construtor um arquivo
+    de texto que define as arestas e vertices ddo grafo desejado
+    """
+
     def __init__(self, arqtexto):
         with open(arqtexto, 'r') as arq:
             self.vertices = {}
 
             regex = re.compile(r'[0-9]+')
 
-            arq.readline()
+            header = regex.findall(arq.readline())
+
+            for number in range(0, int(header[0])):
+                self.vertices[str(number)] = {}
 
             for linha in arq:
                 encontrados = regex.findall(linha)
-                if encontrados[0] not in self.vertices:
-                    self.vertices[encontrados[0]] = {}
-                if encontrados[1] not in self.vertices:
-                    self.vertices[encontrados[1]] = {}
 
-                self.vertices[encontrados[0]][encontrados[1]] = int(
-                    encontrados[2])
-                self.vertices[encontrados[1]][encontrados[0]] = int(
-                    encontrados[2])
+                self.vertices[encontrados[0]][encontrados[1]] = int(encontrados[2])
+                self.vertices[encontrados[1]][encontrados[0]] = int(encontrados[2])
 
     def __str__(self):
         return self.__rep_grafo()
 
+    def dijkstra(self, begin, end, flag_passo):
+        """O metodo calcula a distancia minima entre dois nodos.
 
-    def dijkstra(self, begin, end, passo):
+        Utilizando o algoritmo de dijkstra, o metodo imprime na tela
+        a menor distancia entre os nodos recebidos como parametro.
+        Opcionalmente, imprime cada passo do algoritmo caso o terceiro
+        parametro contenha um True.
+
+        Arguments:
+            begin {int} -- Nodo inicial
+            end {int} -- Nodo final
+            flag_passo {bool} -- Flag booleana para impressao passo a passo
+        """
+
         inicial = str(begin)
         final = str(end)
         visitados = [inicial]
-        #print(list(self.vertices.keys()))
-        unvistidados = list(self.vertices.keys())
-        unvistidados.remove(inicial)
+        nao_visitados = list(self.vertices.keys())
+        nao_visitados.remove(inicial)
 
         dist_inicial = {}
         for vertices in self.vertices:
             dist_inicial[vertices] = INF
 
         dist_inicial[inicial] = 0
-
         for vertice, peso in self.vertices[inicial].items():
             dist_inicial[vertice] = peso
-        
-        #print(self.vertices[inicial].items())
-        #print(dist_inicial)
-        #print(self.vertices)
+
+        if flag_passo == 1:
+            print(Color.BOLD + 'Nodo atual: ' + Color.END + Color.RED + inicial + Color.END)
+            print(Color.BOLD + 'Nodos visitados (S)' + Color.END)
+            print('{' + ', '.join(visitados) + '}')
+            self.__print_tabela(dist_inicial)
 
         while set(visitados) != set(self.vertices):
-            menor = self.__min(unvistidados, dist_inicial)
+            menor = self.__min(nao_visitados, dist_inicial)
             menor_vertice = menor[0]
             menor_distancia = menor[1]
-            if(menor_vertice != ''):
-                if(passo == 1):
-                    print('Nodo atual: '+ menor_vertice)
-                    print('Distancia ate nodo inicial: '+ str(menor_distancia))
-
+            if menor_vertice != '':
                 visitados.append(menor_vertice)
-                if(passo):    
-                    print('Nodos visitados (S)')
-                    print(visitados)
-
-                unvistidados.remove(menor_vertice)
+                nao_visitados.remove(menor_vertice)
 
                 for vertice, peso in self.vertices[menor_vertice].items():
-              	    if(peso + menor_distancia < dist_inicial[vertice]):
+                    if peso + menor_distancia < dist_inicial[vertice]:
                         dist_inicial[vertice] = peso + menor_distancia
 
-                if passo ==1:        
-                    self.__print_tabela(dist_inicial,unvistidados)
-                    print(" ")
-
-                if(menor_vertice == final):
-                	break
+                if flag_passo == 1:
+                    print(Color.BOLD + 'Nodo atual: ' + Color.END + Color.RED + menor_vertice + Color.END)
+                    print(Color.BOLD + 'Distancia ate nodo inicial: ' +
+                          Color.END + Color.BLUE + str(menor_distancia) + Color.END)
+                    print(Color.BOLD + 'Nodos visitados (S)' + Color.END)
+                    print('{' + ', '.join(visitados) + '}')
+                    self.__print_tabela(dist_inicial)
+                if menor_vertice == final:
+                    break
             else:
-            	break
-
-            #print(dist_inicial)
-        print("Menor distancia: " + str(dist_inicial[final]))    
-                
+                break
+        print("\nMenor distancia: " + str(dist_inicial[final]))
 
     def __min(self, unvisitados, dist_inicial):
+        """Retorna uma tupla contendo o vertice mais proximo e a distancia ate ele.
+
+        Dada a lista de vertices nao visitados e a lista de distancia dos vertices
+        ate o inicial, testa os nodos ainda nao visitados de dist_inicial procurando
+        o mais proximo ainda nao visitado.
+
+        Arguments:
+            unvisitados {list} -- Lista de nodos ainda nao visitados
+            dist_inicial {dict} -- Relacao de vertices e distancias em relacao ao inicial
+        """
+
         minimo = INF
-        menor_vertice =  str()
+        menor_vertice = str()
         for vertice in unvisitados:
             dist = int(dist_inicial[vertice])
             if dist < minimo and dist > 0:
                 minimo = dist
                 menor_vertice = vertice
 
+        return (menor_vertice, minimo)
 
-        return [menor_vertice, minimo]
-
-        
     def __rep_grafo(self):
         string = ''
         for nodo, arestas in self.vertices.items():
@@ -105,13 +122,13 @@ class Grafo:
                     '  (peso: ' + str(peso) + ')\n'
         return string
 
-    def __print_tabela(self, dist_inicial, unvisitados):
-    	print("Tabela de distancias ate o nodo inicial")
-    	for variavel, peso in dist_inicial.items():
-            if variavel in unvisitados:
-                string = variavel+ " - "
-                if(peso == INF):
-                	string+= INF_CHAR
-                else:
-            	    string+= str(peso)
-                print(string)
+    def __print_tabela(self, dist_inicial):
+        print(Color.BOLD + "Tabela de distancias ate o nodo inicial:" + Color.END)
+        for variavel, peso in dist_inicial.items():
+            string = Color.RED + variavel + Color.END + " - "
+            if peso == INF:
+                string += str(INF_CHAR)
+            else:
+                string += str(peso)
+            print(string)
+        print(' ')
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from grafo import Grafo
 import os
 import sys
+from grafo import Grafo
+
+"""Implementacao do algoritmo de Dijkstra em Python.
+
+Trabalho final da disciplina de Grafos e Analise Combinatoria
+UFRGS - Instituto de Informatica
+2017/2
+Henrique Correa Pereira da Silva    -   262508
+Maria Clara Machry Jacintho         -   262505
+"""
 
 
-# entrada = Grafo(os.path.abspath(sys.argv[1]))
-entrada = Grafo("inst22n.dat")
+entrada = Grafo(os.path.abspath(sys.argv[1]))
 print(entrada)
-# entrada.dijkstra()
+entrada.dijkstra(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
