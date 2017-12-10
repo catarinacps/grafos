169eb8a210338361963a3adbee6b083234477953
Henrique Silva <henrique.p.silva@outlook.com>
2017-12-10 17:01:16 +0000

    agora com vaaarios comentsss

--- grafo.py
+++ grafo.py
@@ -1,12 +1,18 @@
 import re
 from text_format import Color
 
+"""Pacote da implementacao de um grafo em Python.
+
+Contem todas as dependencias para o funcionamento correto da classe.
+"""
+
+
 INF = 1001
 INF_CHAR = u'\u221E'
 
 
 class Grafo:
-    """A classe Grafo representa um grafo conexo.
+    """A classe Grafo representa um grafo simples.
 
     Deve receber como entrada de seu construtor um arquivo
     de texto que define as arestas e vertices ddo grafo desejado
@@ -46,52 +52,83 @@ class Grafo:
             flag_passo {bool} -- Flag booleana para impressao passo a passo
         """
 
+        # Inicializacao do nodo inicial
         inicial = str(begin)
+        # Inicializacao do nodo final
         final = str(end)
+
+        # Inicializacao da lista de nodos visitados
         visitados = [inicial]
+        # E a inicializacao da lista de nodos nao visitados, para auxilio na execucao
         nao_visitados = list(self.vertices.keys())
         nao_visitados.remove(inicial)
 
+        # A relacao de distancias ao nodo inicial e representada por um dicionario
         dist_inicial = {}
+        # onde cada vertice e uma chave, e o valor desse vertice e a distancia ate o inicial
         for vertices in self.vertices:
             dist_inicial[vertices] = INF
 
         dist_inicial[inicial] = 0
+        # No primeiro passo do algoritmo, ocorre a avaliacao dos nodos vizinhos ao inicial
         for vertice, peso in self.vertices[inicial].items():
+            # e, assim, inicializamos as chaves do dicionario de distancias vizinhas ao inicial
+            # com o peso de suas arestas
             dist_inicial[vertice] = peso
 
+        # Aqui ocorre a impressao do primeiro passo do algoritmo, caso o usuario tenha escolhido
+        # a execucao passo a passo.
         if flag_passo == 1:
             print(Color.BOLD + 'Nodo atual: ' + Color.END + Color.RED + inicial + Color.END)
-            print(Color.BOLD + 'Nodos visitados (S)' + Color.END)
+            print(Color.BOLD + 'Nodos visitados (S):' + Color.END)
             print('{' + ', '.join(visitados) + '}')
             self.__print_tabela(dist_inicial)
 
+        # Enquanto o conjunto de nodos visitados nao for igual ao conjunto de vertices...
         while set(visitados) != set(self.vertices):
             menor = self.__min(nao_visitados, dist_inicial)
             menor_vertice = menor[0]
             menor_distancia = menor[1]
+            # e existir um vertice com uma distancia menor que infinito (representada no codigo
+            # por existir uma distancia nao vazia)...
             if menor_vertice != '':
+                # Comecamos a execucao do passo adicionando o vertice mais proximo do que estavamos
                 visitados.append(menor_vertice)
                 nao_visitados.remove(menor_vertice)
 
+                # E, para cada vertice vizinho ao vertice que estamos olhando
                 for vertice, peso in self.vertices[menor_vertice].items():
+                    # Testamos se o peso dessa aresta + a distancia ate o nodo que estamos e menor
+                    # que a distancia do nodo inicial ate o vertice que essa aresta leva a
                     if peso + menor_distancia < dist_inicial[vertice]:
+                        # Se for menor, redefinimos a distancia do nodo incial ate esse vertice
+                        # com a soma anterior
                         dist_inicial[vertice] = peso + menor_distancia
 
+                # Aqui ocorre a impressao do passo da execucao, se o usuario escolheu tal opcao
                 if flag_passo == 1:
-                    print(Color.BOLD + 'Nodo atual: ' + Color.END + Color.RED + menor_vertice + Color.END)
+                    # Nodo atual da execucao...
+                    print(Color.BOLD + 'Nodo atual: ' + Color.END +
+                          Color.RED + menor_vertice + Color.END)
+                    # distancia desse nodo ate o incial...
                     print(Color.BOLD + 'Distancia ate nodo inicial: ' +
                           Color.END + Color.BLUE + str(menor_distancia) + Color.END)
-                    print(Color.BOLD + 'Nodos visitados (S)' + Color.END)
+                    # e o conjunto de nodos visitados...
+                    print(Color.BOLD + 'Nodos visitados (S):' + Color.END)
                     print('{' + ', '.join(visitados) + '}')
+                    # assim como a tabela de distancias ate o nodo incial
                     self.__print_tabela(dist_inicial)
+                # Finalmente, se o nodo atual for o nodo desejado, terminamos o processamento
                 if menor_vertice == final:
                     break
+            # Se nao existir um nodo mais proximo ao nodo anteriormente executado, tambem terminamos
+            # o processamento
             else:
                 break
+        # E, ao terminar, ocorre a impressao na tela da menor distancia do nodo inicial ao final
         print("\nMenor distancia: " + str(dist_inicial[final]))
 
-    def __min(self, unvisitados, dist_inicial):
+    def __min(self, nao_visitados, dist_inicial):
         """Retorna uma tupla contendo o vertice mais proximo e a distancia ate ele.
 
         Dada a lista de vertices nao visitados e a lista de distancia dos vertices
@@ -99,24 +136,31 @@ class Grafo:
         o mais proximo ainda nao visitado.
 
         Arguments:
-            unvisitados {list} -- Lista de nodos ainda nao visitados
+            nao_visitados {list} -- Lista de nodos ainda nao visitados
             dist_inicial {dict} -- Relacao de vertices e distancias em relacao ao inicial
         """
 
+        # Para encontrar o vertice mais proximo, inicializamos variaveis auxiliares
         minimo = INF
+        # com a distancia "infinita"
         menor_vertice = str()
-        for vertice in unvisitados:
+        # e com o vertice indefinido
+
+        # Para cada vertice ainda nao visitado
+        for vertice in nao_visitados:
             dist = int(dist_inicial[vertice])
+            # Verificamos a distancia ate ele e menor que a distancia atualmente na variavel aux
             if dist < minimo and dist > 0:
                 minimo = dist
                 menor_vertice = vertice
 
+        # Ao testar todos os nodos nao visitados, retornamos o mais proximo
         return (menor_vertice, minimo)
 
     def __rep_grafo(self):
-        string = ''
+        string = Color.CYAN + 'Representacao do grafo:' + Color.END + '\n'
         for nodo, arestas in self.vertices.items():
-            string += nodo + '\n'
+            string += Color.BOLD + nodo + Color.END + '\n'
             for vizinho, peso in arestas.items():
                 string += '  ' + '-> ' + vizinho + \
                     '  (peso: ' + str(peso) + ')\n'
@@ -131,4 +175,4 @@ class Grafo:
             else:
                 string += str(peso)
             print(string)
-        print(' ')
+        print('')
--- main.py
+++ main.py
@@ -7,11 +7,19 @@ from grafo import Grafo
 Trabalho final da disciplina de Grafos e Analise Combinatoria
 UFRGS - Instituto de Informatica
 2017/2
+
 Henrique Correa Pereira da Silva    -   262508
 Maria Clara Machry Jacintho         -   262505
 """
 
 
 entrada = Grafo(os.path.abspath(sys.argv[1]))
-print(entrada)
-entrada.dijkstra(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+try:
+    if sys.argv[4] == '-passo':
+        print(entrada)
+        passo = True
+    else:
+        passo = False
+except IndexError:
+    passo = False
+entrada.dijkstra(int(sys.argv[2]), int(sys.argv[3]), passo)
